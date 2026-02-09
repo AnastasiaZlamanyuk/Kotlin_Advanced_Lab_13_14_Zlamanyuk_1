412a8f2bde15b2631053ee41a251be0fb810819e
AnastasiaZlamanyuk <nastya.zlamanyuk@bk.ru>
2026-02-09 15:33:33 +0300

    Arrays in Kotlin

--- Question.kt
+++ Question.kt
@@ -27,63 +27,60 @@ enum class Difficulty {
 
 interface ProgressPrintable {
     val progressText: String
-    fun printProgressBar()
-}
-
-class Quiz :  ProgressPrintable {
-    override val progressText: String
-        get() = "${answered} of ${total} answered"
-
-    override printProgressBar() {
-        repeat(times = Quiz.answered) { print("▓") }
-        repeat(times = Quiz.total - Quiz.answered) { print("▓") }
+    fun printProgressBar() {
+        repeat(Quiz.answered) { print(" ") }
+        repeat(Quiz.total - Quiz.answered) { print(" ") }
         println()
         println(progressText)
+    }
+}
 
-        fun printQuiz() {
-            question1.let {
-                println(it.questionText)
-                println(it.answer)
-                println(it.difficulty)
-            }
-            println()
-            question2.let {
-                println(it.questionText)
-                println(it.answer)
-                println(it.difficulty)
-            }
-            println()
-            question3.let {
-                println(it.questionText)
-                println(it.answer)
-                println(it.difficulty)
-            }
-            println()
-
-        }
-
+class Quiz : ProgressPrintable {
+    override val progressText: String
+        get() = "${answered} of ${total} answered"
     val question1 = Question<String>(
-        questionText = "Речка спятила с ума - По домам пошла сама. ___",
-        answer = "водопровод",
+        "Речка спятила с ума - По домам пошла сама. ___",
+        "водопровод",
         Difficulty.MEDIUM
     )
     val question2 = Question<Boolean>(
-        questionText = "Небо зелёное. Правда или ложь",
-        answer = false,
+        "Небо зелёное. Правда или ложь",
+        false,
         Difficulty.EASY
     )
     val question3 = Question<Int>(
-        questionText = "Сколько дней между полнолуниями?",
-        answer = 28,
+        "Сколько дней между полнолуниями?",
+        28,
         Difficulty.HARD
     )
+
     companion object StudentProgress {
         var total: Int = 10
         var answered: Int = 3
     }
+
+    fun printQuiz() {
+        question1.let {
+            println(it.questionText)
+            println(it.answer)
+            println(it.difficulty)
+        }
+        println()
+        question2.let {
+            println(it.questionText)
+            println(it.answer)
+            println(it.difficulty)
+        }
+        println()
+        question3.let {
+            println(it.questionText)
+            println(it.answer)
+            println(it.difficulty)
+        }
+        println()
+
+    }
 }
-
-
 
 fun main() {
 //    println(question1.answer)
@@ -96,5 +93,6 @@ fun main() {
 //    Quiz.printProgressBar()
 //    val quiz = Quiz()
 //    quiz.printQuiz()
-    Quiz().apply {printQuiz()}
+    Quiz().printProgressBar()
+    Quiz().apply { printQuiz() }
 }
