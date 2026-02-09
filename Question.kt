a487497dd14ceb66a132ffbc6dbd52d925a6d42c
AnastasiaZlamanyuk <nastya.zlamanyuk@bk.ru>
2026-02-09 15:19:24 +0300

    Use a singleton object

--- Question.kt
+++ Question.kt
@@ -25,25 +25,33 @@ enum class Difficulty {
     HARD
 }
 
-fun main() {
+class Quiz {
     val question1 = Question<String>(
         questionText = "Речка спятила с ума - По домам пошла сама. ___",
         answer = "водопровод",
-        difficulty.MEDIUM
+        Difficulty.MEDIUM
     )
     val question2 = Question<Boolean>(
         questionText = "Небо зелёное. Правда или ложь",
         answer = false,
-        difficulty.EASY
+        Difficulty.EASY
     )
     val question3 = Question<Int>(
         questionText = "Сколько дней между полнолуниями?",
         answer = 28,
-        difficulty.HARD
+        Difficulty.HARD
     )
+    companion object StudentProgress {
+        var total: Int = 10
+        var answered: Int = 3
+    }
+}
+
+fun main() {
 //    println(question1.answer)
 //    println(question2.answer)
 //    println(question3.answer)
-    println(question1.toString())
-
+//    println(question1.toString())
+//    println("${StudentProgress.answered} of ${StudentProgress.total} answered.")
+    println("${Quiz.answered} of ${Quiz.total} answered.")
 }
