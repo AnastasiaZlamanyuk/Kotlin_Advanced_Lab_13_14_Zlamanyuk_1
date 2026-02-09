843a7727b56f55d2811d045f9910566ed2ba20e9
AnastasiaZlamanyuk <nastya.zlamanyuk@bk.ru>
2026-02-09 15:41:51 +0300

    Sets in Kotlin

--- Collection.kt
+++ Collection.kt
@@ -1,17 +1,17 @@
 //val rockPlanets = arrayOf<String>("Mercury", "Venus", "Earth", "Mars")
 //val gasPlanets = arrayOf("Jupiter", "Saturn", "Uranus", "Neptune")
 //val solarSystem = rockPlanets + gasPlanets
-val newSolarSystem = arrayOf(
-    "Mercury",
-    "Venus",
-    "Earth",
-    "Mars",
-    "Jupiter",
-    "Saturn",
-    "Uranus",
-    "Neptune",
-    "Pluto"
-)
+//val newSolarSystem = arrayOf(
+//    "Mercury",
+//    "Venus",
+//    "Earth",
+//    "Mars",
+//    "Jupiter",
+//    "Saturn",
+//    "Uranus",
+//    "Neptune",
+//    "Pluto"
+//)
 
 fun main() {
 //    println(solarSystem[0])
@@ -26,25 +26,44 @@ fun main() {
 //    println(solarSystem[3])
 //    solarSystem[8] = "Pluto"
 //    println(newSolarSystem[8])
-    val solarSystem = mutableListOf(
-        "Mercury", "Venus", "Earth", "Mars",
-        "Jupiter", "Saturn", "Uranus", "Neptune"
-    )
+//    val solarSystem = mutableListOf(
+//        "Mercury", "Venus", "Earth", "Mars",
+//        "Jupiter", "Saturn", "Uranus", "Neptune"
+//    )
 //    println(solarSystem.size)
 //    println(solarSystem[2])
 //    println(solarSystem.get(3))
 //    println(solarSystem.indexOf("Earth"))
 //    println(solarSystem.indexOf("Pluto"))
-    for (planet in solarSystem) {
-        println(planet)
-    }
-    solarSystem.add("Pluto")
-    solarSystem.add(3,"Theia")
-    solarSystem[3] = "Future Moon"
-    println(solarSystem[3])
-    println(solarSystem[9])
-    solarSystem.removeAt(9)
+//    for (planet in solarSystem) {
+//        println(planet)
+//    }
+//    solarSystem.add("Pluto")
+//    solarSystem.add(3,"Theia")
+//    solarSystem[3] = "Future Moon"
+//    println(solarSystem[3])
+//    println(solarSystem[9])
+//    solarSystem.removeAt(9)
+//    println(solarSystem.contains("Pluto"))
+//    println("Future Moon" in solarSystem)
+    val solarSystem = mutableMapOf(
+        "Mercury",
+        "Venus",
+        "Earth",
+        "Mars",
+        "Jupiter",
+        "Saturn",
+        "Uranus",
+        "Neptune"
+    )
+//    println(solarSystem.size)
+//    solarSystem.add("Pluto")
+//    println(solarSystem.size)
+//    println(solarSystem.contains("Pluto"))
+//    ptintln("Pluto" in solarSystem)
+//    solarSystem.add("Pluto")
+//    println(solarSystem.size)
+    solarSystem.remove("Pluto")
+    println(solarSystem.size)
     println(solarSystem.contains("Pluto"))
-    println("Future Moon" in solarSystem)
-
 }
